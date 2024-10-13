40bc9523d781bf4bb64c56a7e64a2684758cb87e
Matthew Oduamafu <mattoduamafu@gmail.com>
2024-10-13 22:41:00 +0000

    basic auth

--- src/rustaceans_api.rs
+++ src/rustaceans_api.rs
@@ -2,9 +2,11 @@ use rocket::{delete, get, post, put};
 use rocket::response::status;
 use rocket::serde::json::json;
 use serde_json::Value;
+use crate::basic_auth::BasicAuth;
 
 #[get("/rustaceans")]
-pub fn get_rustaceans() -> Value {
+pub fn get_rustaceans(auth: BasicAuth) -> Value {
+    println!("{:?}", auth);
     json!([
         { "id": 1, "name": "John Doe", "age": 30 },
         { "id": 1, "name": "Jane Doe", "age": 28 },
@@ -12,21 +14,21 @@ pub fn get_rustaceans() -> Value {
 }
 
 #[get("/rustaceans/<id>")]
-pub fn view_rustacean(id: u32) -> Value {
+pub fn view_rustacean(id: u32, _auth: BasicAuth) -> Value {
     json!({ "id": id, "name": "John Doe", "age": 30 })
 }
 
 #[post("/rustaceans", format = "json")]
-pub fn create_rustacean() -> Value {
+pub fn create_rustacean(_auth: BasicAuth) -> Value {
     json!({ "id": 3, "name": "John Doe", "age": 30 })
 }
 
 #[put("/rustaceans/<id>")]
-pub fn update_rustacean(id: u32) -> Value {
+pub fn update_rustacean(id: u32, _auth: BasicAuth) -> Value {
     json!({ "id": id, "name": "John Doe", "age": 30 })
 }
 
 #[delete("/rustaceans/<_id>")]
-pub fn delete_rustacean(_id: u32) -> status::NoContent {
+pub fn delete_rustacean(_id: u32, _auth: BasicAuth) -> status::NoContent {
     status::NoContent
 }
